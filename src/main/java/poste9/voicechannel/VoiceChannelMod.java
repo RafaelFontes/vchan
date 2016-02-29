9413bd2929007e29a3ddc38c21153465794a6882
Rafael <poste9@gmail.com>
2016-02-29 00:42:26 -0300

    Refactoring the idea using a solid example by coolAlias

--- src/main/java/poste9/voicechannel/VoiceChannelMod.java
+++ src/main/java/poste9/voicechannel/VoiceChannelMod.java
@@ -6,74 +6,83 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.TargetDataLine;
 
-import poste9.voicechannel.dataline.InputThread;
-import poste9.voicechannel.dataline.OutputThread;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import poste9.voicechannel.net.VoicePacketDispatcher;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
 import cpw.mods.fml.common.event.FMLServerStoppedEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import cpw.mods.fml.relauncher.Side;
 
-@Mod(modid = "vchan", name="Voice Channel Mod", version = "0.1", useMetadata = true)
+@Mod(modid = VoiceChannelMod.MOD_ID, name = VoiceChannelMod.MOD_NAME, version = VoiceChannelMod.MOD_VERSION, useMetadata = true)
 public class VoiceChannelMod 
 {
-	AudioFormat format = new AudioFormat(8000.0f, 16, 1, true, true);
-	InputThread input_thread;
-	OutputThread output_thread;
-	Thread read_thread;
-	Boolean is_voice_comm_available;
+	public final static String MOD_ID = "VCM";
+	public final static String MOD_VERSION = "0.1";
+	public final static String MOD_NAME = "Voice Channel Mod";
+
+	public static final Logger logger = LogManager.getLogger(MOD_ID);
 	
+	@Mod.Instance(MOD_ID)
+	public static VoiceChannelMod instance;
+	
+	@SidedProxy(clientSide = "poste9.voicechannel.ModProxyClient", serverSide = "poste9.voicechannel.ModProxy")
+	public static ModProxy proxy;
+
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e)
 	{
-		is_voice_comm_available = false;
-		
-		DataLine.Info input_info = new DataLine.Info(TargetDataLine.class, format);
-		DataLine.Info output_info = new DataLine.Info(SourceDataLine.class, format);
-		
-		if ( !AudioSystem.isLineSupported(input_info) )
-		{
-			System.err.println("Voice communication not supported on this device!");
-		}
-		else
-		{
-			is_voice_comm_available = true;
-			
-			input_thread = new InputThread( input_info, format );
-			output_thread = new OutputThread( output_info, format );
-		}
+		logger.info("Beginning pre-initialization");
+		VoicePacketDispatcher.registerPackets();
 	}
 	
 	@EventHandler
 	public void init(FMLInitializationEvent e)
 	{
-		if ( is_voice_comm_available )
+		logger.info("Beginning initialization");
+		
+		// Register renders here
+		
+		ModEventHandler events = new ModEventHandler();
+		MinecraftForge.EVENT_BUS.register(events);
+		
+		/*if ( is_voice_comm_available )
 		{
-			output_thread.start();
-			
-			input_thread.set_output_thread( output_thread );
-			
 			input_thread.start();
-		}
+			sound_output.start();
+		}*/
 	}
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent e)
 	{
+		logger.info("Post initialization");
+
 	}
 	
 	@EventHandler
 	public void serverStopped(FMLServerStoppedEvent e)
 	{
-		System.out.println("server stopped!");
+		logger.info("Server stopped");
 	}
 	
 	@EventHandler
 	public void serverStarted(FMLServerStartedEvent e)
 	{
-		System.out.println("server started!");
-		System.out.println("Start server thread here!");
+		logger.info("Server started");
 	}
 }
